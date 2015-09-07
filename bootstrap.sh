5a510370fa75aa817d19ba550551125b66282894
Krzysztof Magosa <krzysztof.magosa@performgroup.com>
2015-09-07 17:25:50 +0200

    add missing modules to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,11 @@
 
 [ -d /opt/ansible ] && exit 0
 
+sudo easy_install pip
+sudo pip install pyyaml
+sudo pip install jinja2
+sudo pip install six
+
 sudo mkdir -p /opt/ansible
 sudo git clone https://github.com/ansible/ansible.git /opt/ansible --recursive
 sudo bash /opt/ansible/hacking/env-setup
